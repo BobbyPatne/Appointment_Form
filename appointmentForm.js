ccd91d7db3426d701645513f684d2a33134a7d54
BobbyPatne <165859487+BobbyPatne@users.noreply.github.com>
2024-07-29 13:31:18 +0530

    Add files via upload

---
+++ appointmentForm.js
@@ -0,0 +1,125 @@
+import { LightningElement,wire,api } from 'lwc';
+
+import createAppointment from '@salesforce/apex/AppointmentController.createAppointment';
+import getAppointmentSlots from '@salesforce/apex/AppointmentController.getAppointmentSlots';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+
+export default class AppointmentForm extends LightningElement {
+   selectedDate;
+   selectedTime;
+   selectedContact;
+   selectedSubject;
+   selectedDescription;
+   selectedName;
+   newArray =[];
+   slotCheck= true;
+  
+
+   handleName(event){
+    this.selectedName = event.target.value;
+   }
+ 
+   handleDescription(event){
+    this.selectedDescription = event.target.value;
+   }
+
+   handleTime(event){
+    this.selectedTime = event.target.value;
+    
+        }
+    handleDate(event){
+        this.selectedDate  = event.target.value;
+    }
+    handleContact(event){
+     this.selectedContact = event.target.value;
+     console.log('selected contact', this.selectedContact);
+     console.log('selected contact json', JSON.stringify(event.target.value));
+    }
+    handleSubject(event){
+        this.selectedSubject = event.target.value;
+    }
+
+
+    checkHandler(){
+        let formattedTime2 =this.createFormattedTime(this.selectedTime);
+        getAppointmentSlots({aptDate:this.selectedDate, TimeInteger:formattedTime2})
+        .then(result=>{
+            if(result){
+                this.slotCheck = false;
+                this.showToast('Slot is Available', 'you can book this Slot','success'); 
+            } else{
+                this.slotCheck = true;
+                this.showToast('Sorry Slot is not Available', 'you can not book this Slot','error'); 
+            }
+           
+           
+    })
+         .catch(error=>{
+           console.log('Error found while creating',error);
+           
+         })
+    }
+
+submitHandler(){
+const formattedTime = this.createFormattedTime(this.selectedTime);
+
+const contactField = this.template.querySelector(".contactclass");
+console.log('values is ',contactField);
+if(contactField.value === undefined){
+    contactField.setCustomValidity("Please Select the Contact");
+} else{
+    contactField.setCustomValidity("");
+    
+    createAppointment({ AppointmentDate:this.selectedDate, AppointmentTime:formattedTime,
+        Sub:this.selectedSubject, Descr:this.selectedDescription,contactId:this.selectedContact,Name:this.selectedName   
+    })
+        .then(result=>{
+            console.log(result);
+            this.showToast('Successful', result,'success');           this.resetHandle();
+            this.slotCheck = true;
+           
+           // window.location.reload();
+    })
+         .catch(error=>{
+           console.log('Error found while creating',error);
+           this.showToast('Error',result,'error');
+         })
+}
+
+contactField.reportValidity();
+
+}
+
+resetHandle(){
+    this.selectedDate ='';
+   this.selectedTime='';
+   this.selectedContact='';
+   this.selectedSubject='';
+   this.selectedDescription='';
+   this.selectedName='';
+}
+
+
+
+
+createFormattedTime(timestring){
+    let [time, ms] = timestring.split('.'); // ms
+    let [hh, mm, ss] = time.split(':');
+     this.newArray = [parseInt(hh, 10), parseInt(mm, 10), parseInt(ss, 10), parseInt(ms, 10)];
+     return  this.newArray;
+   
+    }
+
+showToast(Header,Subheader,Way) {
+    const eve = new ShowToastEvent({
+        title: Header,
+        message: Subheader,
+        variant: Way,
+        //mode: 'dismissable'
+    })
+    this.dispatchEvent(eve);
+}
+  
+
+}
